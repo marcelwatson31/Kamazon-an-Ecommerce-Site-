4e2a97350becff7b640c7a5955ac9b1693f5e020
marc watkins <marcelwatson31@yahoo.com>
2020-11-24 13:52:50 -0600

    static root changed

--- ecomsite/settings.py
+++ ecomsite/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/store/images')
